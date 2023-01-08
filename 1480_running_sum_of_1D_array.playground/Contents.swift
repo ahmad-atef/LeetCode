11be80c5500edfdf4d46e4d525f9b837e933b8aa
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2023-01-08 16:18:59 +0200

    Adding better solution and in-place solution.

--- 1480_running_sum_of_1D_array.playground/Contents.swift
+++ 1480_running_sum_of_1D_array.playground/Contents.swift
@@ -13,12 +13,33 @@ class Solution {
         }
         return runningSum
     }
+    
+    func betterSolution(_ nums: [Int]) -> [Int] {
+        var result: [Int] = []
+        for i in 0 ..< nums.count {
+            var previousSum = result.last ?? 0
+            var runningSum = previousSum + nums[i]
+            result.append(runningSum)
+        }
+        return result
+    }
+    
+    func overrideSolution(_ nums: inout [Int]) -> [Int] {
+        for i in 1 ..< nums.count {
+            nums[i] += nums[i - 1]
+        }
+        return nums
+    }
 }
 
 
-let nums = [3,1,2,10,1]
+var nums = [3,1,2,10,1]
 
 let solution = Solution()
 let result = solution.runningSum(nums)
+let betterSolutionResult = solution.betterSolution(nums)
+let overrideSolution = solution.overrideSolution(&nums)
 
 print(result)
+print(betterSolutionResult)
+print(overrideSolution)
